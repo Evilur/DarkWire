689379fd3b5196eac15d5bbb41d9b20f6bb3c8d6
Andrew Shershunov <mail@evilur.ru>
2026-02-17 00:08:55 +0300

    refactor: Update the Path class for this project

--- src/util/path.cpp
+++ src/util/path.cpp
@@ -1,7 +1,6 @@
 #include "path.h"
 
-void Path::Init(){
+void Path::Init() {
     /* Create all necessary directories */
     fs::create_directories(CONFIG_DIR);
-    fs::create_directories(LOG_DIR);
 }
--- src/util/path.h
+++ src/util/path.h
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #endif
 
+/* Create a shorcut for the std::filesystem namespace */
 namespace fs = std::filesystem;
 
 class Path final {
@@ -20,25 +21,7 @@ public:
 
 /* If we are compiling for Unix based */
 #if defined(__unix__) || defined(__unix) || defined(unix)
-    /* Get the user home directory */
-    static inline const fs::path HOME_DIR = getpwuid(getuid())->pw_dir;
-
     /* Get the config directory */
-    static inline const fs::path CONFIG_DIR = HOME_DIR / ".config/Ping";
-
-    /* Get the log directory */
-    static inline const fs::path LOG_DIR = HOME_DIR / ".cache/Ping";
-
+    static inline const fs::path CONFIG_DIR = "/etc/darkwire";
 #endif
-    /* Get the config file path */
-    static inline const fs::path CONFIG_FILE = CONFIG_DIR / "settings.ini";
-
-    /* Get the config file backup path */
-    static inline const fs::path CONFIG_FILE_BAK = CONFIG_DIR / "settings.bak";
-
-    /* Get the ultralight log file path */
-    static inline const fs::path ULTRALIGHT_LOG = LOG_DIR / "ultralight.log";
-
-    /* Get the basic log file */
-    static inline const fs::path LOG_FILE = LOG_DIR / "ping.log";
 };
