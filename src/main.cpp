2fc58cbf393963a57d0c4db8ad211f1db0c50223
Andrew Shershunov <mail@evilur.ru>
2026-02-16 21:37:51 +0300

    feat: Add implementation of 'genkey' and 'pubkey' arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include "net/tun.h"
 #include "util/logger.h"
 
 #include <cstring>
 #include <exception>
+#include <sodium.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+    #include <io.h>
+    #define ISATTY _isatty
+    #define FILENO _fileno
+#else
+    #include <unistd.h>
+    #define ISATTY isatty
+    #define FILENO fileno
+#endif
 
 static void on_terminate();
 
@@ -11,8 +23,6 @@ static int print_help();
 static int genkey();
 
 static int pubkey();
-
-static int keypair();
 
 static int handle_config(const char* filename);
 
@@ -23,13 +33,18 @@ int main(const int argc, const char* const* const argv) {
     /* If there is no arguments */
     if (argc <= 1) return print_help();
 
+    /* Init the libsodium */
+    if (sodium_init() == -1) {
+        FATAL_LOG("Failed to initiate the libsodium");
+        return -1;
+    }
+
     /* Read the argument */
     const char* const arg = argv[1];
     if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0)
         return print_help();
     if (strcmp(arg, "genkey") == 0) return genkey();
     if (strcmp(arg, "pubkey") == 0) return pubkey();
-    if (strcmp(arg, "keypair") == 0) return keypair();
     return handle_config(arg);
 }
 
@@ -51,13 +66,95 @@ static void on_terminate() {
 }
 
 static int print_help() {
+    return -1;
+}
+
+static int genkey() {
+    /* Create buffers for keys */
+    constexpr int key_size = 32;
+    unsigned char secretkey_buffer[key_size];
+    unsigned char publickey_buffer[key_size];
+
+    /* Generate the secret key */
+    randombytes_buf(secretkey_buffer, key_size);
+
+    /* Generate the public key and do a key clamping */
+    crypto_scalarmult_base(publickey_buffer, secretkey_buffer);
+
+    /* Create a buffer for the base64 key representations */
+    const unsigned long base64_size = sodium_base64_encoded_len(
+        key_size,
+        sodium_base64_VARIANT_ORIGINAL
+    );
+    char* const base64_buffer = new char[base64_size + 1];
+    base64_buffer[base64_size] = '\0';
+
+    /* Convert the secret key to the base64 form */
+    sodium_bin2base64(base64_buffer,
+                      base64_size,
+                      secretkey_buffer,
+                      key_size,
+                      sodium_base64_VARIANT_ORIGINAL);
+
+    /* Print the base64 secret key representation */
+    printf("Secret key: %s\n", base64_buffer);
+
+    /* Convert the public key to the base64 form */
+    sodium_bin2base64(base64_buffer, base64_size,
+                      publickey_buffer, key_size,
+                      sodium_base64_VARIANT_ORIGINAL);
+
+    /* Print the base64 public key representation */
+    printf("Public key: %s\n", base64_buffer);
+
+    /* Return the success code */
+    delete[] base64_buffer;
     return 0;
 }
 
-static int genkey() { }
+static int pubkey() {
+    /* Create buffers for keys */
+    constexpr int key_size = 32;
+    unsigned char secretkey_buffer[key_size];
+    unsigned char publickey_buffer[key_size];
 
-static int pubkey() { }
+    /* Create a buffer for the base64 key representations */
+    const unsigned long base64_size = sodium_base64_encoded_len(
+        key_size,
+        sodium_base64_VARIANT_ORIGINAL
+    );
+    char* const base64_buffer = new char[base64_size + 1];
+    base64_buffer[base64_size] = '\0';
 
-static int keypair() { }
+    /* Read the STDIN */
+    if (ISATTY(FILENO(stdin))) printf("Enter the secret key: ");
+    if (fgets((char*)base64_buffer, (int)base64_size, stdin) == nullptr) {
+        FATAL_LOG("Failed to read the STDIN");
+        return -1;
+    }
 
-static int handle_config(const char* const filename) { }
+    /* Decode the base64 */
+    sodium_base642bin(secretkey_buffer, key_size,
+                      base64_buffer, base64_size,
+                      nullptr, nullptr, nullptr,
+                      sodium_base64_VARIANT_ORIGINAL);
+
+    /* Generate the public key and do a key clamping */
+    crypto_scalarmult_base(publickey_buffer, secretkey_buffer);
+
+    /* Convert the public key to the base64 form */
+    sodium_bin2base64(base64_buffer, base64_size,
+                      publickey_buffer, key_size,
+                      sodium_base64_VARIANT_ORIGINAL);
+
+    /* Print the base64 public key representation */
+    printf("%s\n", base64_buffer);
+
+    /* Return the success code */
+    delete[] base64_buffer;
+    return 0;
+}
+
+static int handle_config(const char* const filename) {
+    return -1;
+}
