6a58568174ca04ae76ccca644d7d140dcb6bdf8b
Andrew Shershunov <mail@evilur.ru>
2026-02-17 23:50:10 +0300

    feat: Read the config file in the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "util/logger.h"
+#include "container/dictionary.h"
+#include "type/string.h"
+#include "util/path.h"
 
+#include <cstdio>
 #include <cstring>
 #include <exception>
+#include <fstream>
 #include <sodium.h>
-#include <stdio.h>
-#include <string.h>
 
 #ifdef _WIN32
     #include <io.h>
@@ -24,7 +27,7 @@ static int genkey();
 
 static int pubkey();
 
-static int handle_config(const char* filename);
+static int handle_config(const char* name);
 
 int main(const int argc, const char* const* const argv) {
     /* Bind the 'on_terminate' handler */
@@ -38,6 +41,9 @@ int main(const int argc, const char* const* const argv) {
         FATAL_LOG("Failed to initiate the libsodium");
         return -1;
     }
+
+    /* Init static classes */
+    Path::Init();
 
     /* Read the argument */
     const char* const arg = argv[1];
@@ -173,6 +179,90 @@ static int pubkey() {
     return 0;
 }
 
-static int handle_config(const char* const filename) {
-    return -1;
+static int handle_config(const char* const name) {
+    /* Set the suffix */
+    constexpr const char suffix[] = ".conf";
+
+    /* Check if we has the '.conf' suffix */
+    const bool has_suffix =
+        strcmp(name + strlen(name) - sizeof(suffix) + 1, suffix) == 0;
+
+    /* Build the file name */
+    String filename = has_suffix ? String(name) : String(name) + suffix;
+
+    /* Check such a file in the working directory */
+    const fs::path config_path = fs::exists((const char*)filename)
+        ? (const char*)filename : (Path::CONFIG_DIR / (const char*)filename);
+
+    /* If there is no such a config file anyway */
+    if (!fs::exists(config_path)) {
+        FATAL_LOG("No such a config file: '%s' or '%s'",
+                  (const char*)filename,
+                  fs::absolute(config_path).c_str());
+        return -1;
+    }
+
+    /* Read the config file */
+    std::ifstream config = std::ifstream(config_path);
+    if (!config.is_open()) {
+        FATAL_LOG("Failed to open the config file '%s'",
+                  fs::absolute(config_path).c_str());
+        return -1;
+    }
+
+    /* Read the config file line by line */
+    constexpr int BUFFER_SIZE = 1024 * 8;
+    char line_buffer[BUFFER_SIZE];
+    Dictionary<String, const char*>* current_section_dict = nullptr;
+    Dictionary<String, Dictionary<String, const char*>*> config_dict(4);
+    while (config.getline(line_buffer, BUFFER_SIZE)) {
+        /* Delete the comments */
+        char* comment_ptr = strchr(line_buffer, '#');
+        if (comment_ptr != nullptr) *comment_ptr = '\0';
+
+        /* Get the first non-space char */
+        char* line_ptr = line_buffer;
+        while(*line_ptr == ' ') ++line_ptr;
+
+        /* Skip the blank lines */
+        if (*line_ptr == '\0') continue;
+
+        /* If there is a section line */
+        if (*line_ptr == '[') {
+            /* Read the section name */
+            char section_name[16];
+            sscanf(line_ptr, "[%15[^]]]", section_name);
+
+            /* Put the parameters dictionary to the sectiona dictionary */
+            current_section_dict = new Dictionary<String, const char*>(16);
+            config_dict.Put(section_name, current_section_dict);
+            TRACE_LOG("Reading the config section '%s'", section_name);
+            continue;
+        }
+
+        /* If there isn't any section */
+        if (current_section_dict == nullptr) continue;
+
+        /* Save the parameter key */
+        char* const parameter_key = line_ptr;
+
+        /* Get the end of the parameter key */
+        char* parameter_key_end = strchr(parameter_key, '=');
+        if (parameter_key_end == nullptr) continue;
+        char* parameter_value_ptr = parameter_key_end + 1;
+        --parameter_key_end;
+        while (*parameter_key_end == ' ') *parameter_key_end = '\0';
+
+        /* Save the parameter value to the heap */
+        while (*parameter_value_ptr == ' ') ++parameter_value_ptr;
+        char* parameter_value = new char[strlen(parameter_value_ptr)];
+        strcpy(parameter_value, parameter_value_ptr);
+
+        /* Save the parameter to the dictionary */
+        current_section_dict->Put(parameter_key, parameter_value);
+        TRACE_LOG("Config parameter has been saved: %s = %s",
+                  parameter_key, parameter_value);
+    }
+
+    return 0;
 }
