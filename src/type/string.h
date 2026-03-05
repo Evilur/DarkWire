890924886aca2446c9cad96893efa09a373c8e42
Andrew Shershunov <the.evilur@gmail.com>
2026-03-05 15:50:08 +0300

    fix(String): Some minor fixes

--- src/type/string.h
+++ src/type/string.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdio>
+#include <cstring>
 #include <string_view>
 
 class String final {
@@ -54,4 +56,138 @@ public:
     static T ToInt(const char* str) noexcept;
 };
 
-#include "string_imp.h"
+inline String::String(const unsigned long str_size) :
+    _str(new char[str_size + 1]), _size(str_size) {
+    _str[str_size] = '\0';
+}
+
+inline String::String(const char* const str) : String(str, strlen(str)) { }
+
+inline String::String(const char* str, const unsigned long str_size) :
+    String(str_size) {
+    if (str != nullptr) memcpy(_str, str, str_size);
+}
+
+inline String::String(const String& other) :
+    _str(new char[other._size + 1]), _size(other._size) {
+    /* Assign a new data */
+    memcpy(_str, other._str, other._size + 1);
+}
+
+inline String& String::operator=(const String& other) {
+    /* Self-assignment check */
+    if (this == &other) return *this;
+
+    /* Delete previous data */
+    delete[] _str;
+
+    /* Nullptr check */
+    if (other._str == nullptr) {
+        /* Assign an empty string */
+        _str = new char[1];
+        _str[0] = '\0';
+        _size = 0;
+    } else {
+        /* Assign a new data */
+        _str = new char[other._size + 1];
+        memcpy(_str, other._str, other._size + 1);
+        _size = other._size;
+    }
+
+    return *this;
+}
+
+inline String::String(String&& other) noexcept :
+    _str(other._str), _size(other._size) {
+    other._str = nullptr;
+    other._size = 0;
+}
+
+inline String& String::operator=(String&& other) noexcept {
+    /* Self-move check */
+    if (this == &other) return *this;
+
+    /* Delete previous data */
+    delete[] _str;
+
+    /* Assign a new data */
+    _str = other._str;
+    _size = other._size;
+
+    /* Null the old object */
+    other._str = nullptr;
+    other._size = 0;
+    return *this;
+}
+
+inline String String::operator+(const char symbol) const {
+    String result(_str, _size + 1);
+    result._str[_size] = symbol;
+    return result;
+}
+
+inline String String::operator+(const char* const other) const {
+    /* Nullptr check */
+    if (other == nullptr) return String(_str, _size);
+
+    const unsigned long other_size = strlen(other);
+    String result(_size + other_size);
+    memcpy(result._str, _str, _size);
+    memcpy(result._str + _size, other, other_size);
+    return result;
+}
+
+inline bool String::operator==(const String& other) const noexcept {
+    return strcmp(_str, other._str) == 0;
+}
+
+inline bool String::operator==(const char* const other) const noexcept {
+    return strcmp(_str, other) == 0;
+}
+
+inline bool String::operator!=(const String& other) const noexcept {
+    return strcmp(_str, other._str) != 0;
+}
+
+inline bool String::operator!=(const char* const other) const noexcept {
+    return strcmp(_str, other) != 0;
+}
+
+inline String::operator const char*() const noexcept { return _str; }
+
+inline String::operator char*() const noexcept { return _str; }
+
+inline String::operator std::string_view()
+const noexcept { return { _str, _size }; }
+
+inline String::~String() noexcept { delete[] _str; }
+
+inline const char* String::CStr() const noexcept { return _str; }
+
+template <typename T>
+T String::ToInt(const char* str) noexcept {
+    /* Check for integer */
+    static_assert(std::is_integral_v<T>, "<T> must be the integer");
+
+    /* If the integer type is unsigned */
+    if constexpr (std::is_unsigned_v<T>) {
+        T result = 0;
+        while (*str >= '0' && *str <= '9') {
+            result *= 10;
+            result += *str++ - '0';
+        }
+        return result;
+    /* If the integer type is signed */
+    } else {
+        using unsigned_t = std::make_unsigned_t<T>;
+        if (*str == '-') return -ToInt<unsigned_t>(str + 1);
+        return ToInt<unsigned_t>(str);
+    }
+}
+
+template <typename... Args>
+String String::Format(const char* const format, Args... args) {
+    String result((unsigned long)snprintf(nullptr, 0, format, args...));
+    sprintf(result._str, format, args...);
+    return result;
+}
