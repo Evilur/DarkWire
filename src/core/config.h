48fdf0c33e1a1021de6fdfce23681c7e55f0c28b
Andrew Shershunov <the.evilur@gmail.com>
2026-03-08 01:48:54 +0300

    refactor: Change default MTU to 1420

--- src/core/config.h
+++ src/core/config.h
@@ -19,8 +19,7 @@ public:
         static inline LinkedList<String> pre_down;
         static inline LinkedList<String> post_down;
         static inline short listen = 0;
-        static inline int mtu = sizeof(TransferData::payload) -
-                                crypto_aead_chacha20poly1305_ietf_ABYTES;
+        static inline int mtu = 1420;
     };
 
     struct Server final {
