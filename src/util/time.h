506e7be90d5c3500b8fc2113896551a6361881ce
Andrew Shershunov <the.evilur@gmail.com>
2026-04-04 16:56:28 +0300

    feat: Add wrapper methods for sleep functions

--- src/util/time.h
+++ src/util/time.h
@@ -2,6 +2,12 @@
 
 #include "util/macro.h"
 
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
 #include <chrono>
 
 /**
@@ -11,19 +17,71 @@
 #include <cstdint>
 class Time final {
 public:
+    static uint64_t Seconds() noexcept;
+
     static uint64_t Nanoseconds() noexcept;
 
     static uint64_t Delta(uint64_t time1, uint64_t time2) noexcept;
+
+    static void Sleep(uint64_t seconds) noexcept;
+
+    static void NanoSleep(uint64_t nanoseconds) noexcept;
+
+    static void WaitFor(uint64_t timestamp) noexcept;
+
+    static void NWaitFor(uint64_t timestamp) noexcept;
 };
+
+FORCE_INLINE uint64_t Time::Seconds() noexcept {
+    timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return (uint64_t)now.tv_sec;
+}
 
 FORCE_INLINE uint64_t Time::Nanoseconds() noexcept {
     timespec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
-    return (uint64_t)now.tv_sec * 1'000'000'000ULL + (uint64_t)now.tv_nsec;
+    return ((uint64_t)now.tv_sec * 1'000'000'000ULL) + (uint64_t)now.tv_nsec;
 }
 
 FORCE_INLINE uint64_t Time::Delta(const uint64_t time1,
                                   const uint64_t time2) noexcept {
     if (time1 > time2) return time1 - time2;
     return time2 - time1;
+}
+
+FORCE_INLINE void Time::Sleep(const uint64_t seconds) noexcept {
+#ifdef _WIN32
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+#else
+    const timespec timeseconds = {
+        .tv_sec = time_t(seconds),
+        .tv_nsec = 0L
+    };
+    nanosleep(&timeseconds, nullptr);
+#endif
+}
+
+FORCE_INLINE void Time::NanoSleep(const uint64_t nanoseconds) noexcept {
+#ifdef _WIN32
+    std::this_thread::sleep_for(std::chrono::nanoseconds(nanoseconds));
+#else
+    const timespec timeseconds = {
+        .tv_sec = time_t(nanoseconds / 1'000'000'000L),
+        .tv_nsec = long(nanoseconds % 1'000'000'000L)
+    };
+    nanosleep(&timeseconds, nullptr);
+#endif
+}
+
+FORCE_INLINE void Time::WaitFor(const uint64_t timestamp) noexcept {
+    const uint64_t current_timestamp = Time::Seconds();
+    if (current_timestamp > timestamp) return;
+    Time::Sleep(timestamp - current_timestamp);
+}
+
+FORCE_INLINE void Time::NWaitFor(const uint64_t timestamp) noexcept {
+    const uint64_t current_timestamp = Time::Nanoseconds();
+    if (current_timestamp > timestamp) return;
+    Time::NanoSleep(timestamp - current_timestamp);
 }
